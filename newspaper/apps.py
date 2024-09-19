c231557ac9a69728d3aaa4852e5b25febeeb8afc
Halyna Rohatska <rohatskahr@gmail.com>
2024-09-19 13:11:04 +0300

    Refactored code: replaced all single quotes with double quotes

--- newspaper/apps.py
+++ newspaper/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NewapaperConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'newspaper'
+class NewspaperConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "newspaper"
