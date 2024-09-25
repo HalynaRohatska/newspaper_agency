e683096e9f2e6e3a2c73cc188a8504aacb403881
Halyna Rohatska <rohatskahr@gmail.com>
2024-09-25 19:51:58 +0300

    add secret key to .env file

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -24,7 +24,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0@*!qg26q2ggp&&pnye1%3ejd1fwi+(b5d7$v_3_62()u(+ysk"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
